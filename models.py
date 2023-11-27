191f2865ca487dad161911cc9f9992e5130c6fe5
bilgecelikk <jbfmf10@gmail.com>
2023-11-27 03:16:36 +0300

    object model added into models.py

---
+++ models.py
@@ -0,0 +1,37 @@
+class object:
+    def __init__(self, objectid, accessioned, accessionnum, locationid, title, displayDate, beginYear,
+                 endYear, visualBrowserTimeSpan, medium, dimensions, inscription, markings,
+                 attributionInverted, attribution, provenanceText, creditLine, classification,
+                 subClassification, visualBrowserClassification, parentid, isVirtual,
+                 departmentabbr, portfolio, series, volume, watermarks, lastDetectedModification,
+                 wikidataid, customPrintURL):
+        self.objectid = objectid
+        self.accessioned = accessioned
+        self.accessionnum = accessionnum
+        self.locationid = locationid
+        self.title = title
+        self.displayDate = displayDate
+        self.beginYear = beginYear
+        self.endYear = endYear
+        self.visualBrowserTimeSpan = visualBrowserTimeSpan
+        self.medium = medium
+        self.dimensions = dimensions
+        self.inscription = inscription
+        self.markings = markings
+        self.attributionInverted = attributionInverted
+        self.attribution = attribution
+        self.provenanceText = provenanceText
+        self.creditLine = creditLine
+        self.classification = classification
+        self.subClassification = subClassification
+        self.visualBrowserClassification = visualBrowserClassification
+        self.parentid = parentid
+        self.isVirtual = isVirtual
+        self.departmentabbr = departmentabbr
+        self.portfolio = portfolio
+        self.series = series
+        self.volume = volume
+        self.watermarks = watermarks
+        self.lastDetectedModification = lastDetectedModification
+        self.wikidataid = wikidataid
+        self.customPrintURL = customPrintURL
