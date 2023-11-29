81f152747f7da3f779f52eb1796c4038ce870651
hacerakinci <hacerakinci07@gmail.com>
2023-11-29 23:03:34 +0300

    Updated files for MVC architecture

--- location.py
+++
@@ -1,6 +0,0 @@
-class Location:
-    def __init__(self, building, location_key, description = "") -> None:
-        self.building = building
-        self.location_key = location_key
-        self.location_id = None
-        self.description = description
