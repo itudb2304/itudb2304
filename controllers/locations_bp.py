de8487fc3863d610d6282c0c79b21d2c4ccfb27a
Hacer Akıncı <108008177+hacerakinci@users.noreply.github.com>
2023-12-06 15:24:12 +0300

    Updated location class and LocationRepository functions for building, floor and room design (#19)
    * Added home_page url to navbar
    
    * Updated location class and LocationRepository functions for building, floor and room design

--- controllers/locations_bp.py
+++ controllers/locations_bp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from repository.locations_repository import LocationsRepository
 from repository.locations_repository import Location
 
@@ -17,10 +17,17 @@ def locations_bp(connection):
     def locations_page():
         locations = repository.get_buildings()
         return render_template("locations.html", locations=locations)
+            
+
+    @locations.route('/<floor_id>')
+    def floor_page(floor_id):
+        rooms = repository.get_rooms(floor_id)
+        return render_template("location.html", rooms=rooms)
     
-    @locations.route('/<location>')
-    def location_page(location):  
-        floors = repository.get_floors(location)
-        return render_template("location.html", floors=floors)
+    @locations.route('/<location_id>')
+    def location_page(location_id):
+        locationkey = repository.get_locationkey(location_id)        
+        rooms = repository.get_rooms(locationkey)
+        return render_template("location.html", rooms=rooms)
 
     return locations
--- repository/locations_repository.py
+++ repository/locations_repository.py
@@ -1,7 +1,7 @@
 class Location:
-    def __init__(self, building) -> None:
-        self.building = building
-        self.floors = []
+    def __init__(self, name, objects) -> None:
+        self.name = name
+        self.objects = objects
 
 class LocationsRepository:
     def __init__(self, connection) -> None:
@@ -14,11 +14,12 @@ class LocationsRepository:
                 SELECT DISTINCT p.description, p.locationkey FROM preferred_locations AS p
                 JOIN locations AS l
                 ON l.site = p.description 
-                WHERE p.locationtype = "building"; 
-            '''
+                WHERE p.locationtype = "building";'''
             cursor.execute(query)
             for building, id in cursor:
-                buildings.append((building, id))
+                buildings.append([building, id])
+        for building in buildings:
+            building.append(self.get_floors(building[1]))
         return buildings
     
     def get_floors(self, building_id):
@@ -29,6 +30,48 @@ class LocationsRepository:
                 WHERE p.partof = %s;
             '''
             cursor.execute(query, [building_id])
-            for description, floors_id in cursor:
-                floors.append((description, floors_id))
+            for description, floor_id in cursor:
+                floors.append((description, floor_id))
         return floors
+    
+    def get_rooms(self, floor_id):
+        rooms = []
+        with self.connection.cursor() as cursor:
+            query = '''
+                SELECT p.description, p.locationkey FROM preferred_locations as p
+                WHERE p.partof = %s;
+            '''
+            cursor.execute(query, [floor_id])
+            for description, room_id in cursor:
+                rooms.append((description, room_id))
+        return rooms
+    
+    def get_room(self, room_id):
+        objects = []
+        with self.connection.cursor() as cursor:
+            query = '''
+                SELECT p.description FROM preferred_locations AS p
+                WHERE p.locationkey = %s;
+            '''
+            cursor.execute(query, [room_id])
+            room_name = cursor.fetchone()
+            query = '''
+                SELECT o.objectid FROM objects AS o
+                WHERE o.locationid = %s;
+            '''
+            cursor.execute(query, [room_id])
+            for object_id in cursor:
+                objects.append((object_id))
+        room = Location(room_name, objects)
+        return room
+    
+    def get_locationkey(self, location_id):
+        locationkey = ""
+        with self.connection.cursor() as cursor:
+            query = ''' 
+                SELECT l.room FROM locations AS l
+                WHERE l.locationid = %s; '''
+            cursor.execute(query, [location_id])
+            for key in cursor:
+                locationkey = key
+        return locationkey
