8a0ae1997d375e28d5fc0c6071e083edc139290c
Ceren Yaşar <yasarcereen@gmail.com>
2023-12-19 22:08:41 +0300

    constituents repository + ui additions

--- controllers/constituents_bp.py
+++ controllers/constituents_bp.py
@@ -1,27 +1,24 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from repository.constituent_repository import ConstituentRepository
 
-def constituents_bp(cursor):
+def constituents_bp(connection):
     constituents = Blueprint(
         'constituents',
         __name__,
-        template_folder='templates/',
+        template_folder='../templates/constituents',
         static_folder='static/',
         url_prefix="/constituents"
     )
 
-    repository = ConstituentRepository(cursor=cursor)
+    repository = ConstituentRepository(connection=connection)
 
-    @constituents.route('/')
+    @constituents.route('/', methods=['GET', 'POST'])
     def constituents_page():
         constituents = repository.get_all_constituents()
 
-        if request.method == 'GET':
-            req = request.args.get('q')
-            if (req):
-                return redirect( url_for('.constituent_by_name', name=req) )
-            else:
-                return render_template('constituents.html', constituents=constituents)
+        if request.method == 'POST':
+            req = request.form['constituent-search']
+            return redirect( url_for('.constituent_by_name', name=req) )
 
         return render_template('constituents.html', constituents=constituents)
     
@@ -34,5 +31,11 @@ def constituents_bp(cursor):
     def constituent_by_name(name: str):
         constituents = repository.get_constituents_by_name(name=name)
         return render_template('constituents.html', constituents=constituents)
+    
+    # @constituents.route('/add', methods=['GET','POST'])
+    # def add_constituent():
+    #     attributes = request.args.get('list')
+    #     return render_template('constituents_add.html')
+
 
     return constituents
