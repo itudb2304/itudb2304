e71d01651bb9e1ef6d580c1071c17664eb049f24
unknown <aydangunaydin@hotmail.com>
2023-12-20 20:37:04 +0300

    media for constituents page

---
+++ controllers/constituents_bp.py
@@ -0,0 +1,80 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from repository.constituent_repository import ConstituentRepository
+from repository.media_repository import MediaRepository
+
+def constituents_bp(connection):
+    constituents = Blueprint(
+        'constituents',
+        __name__,
+        template_folder='../templates/constituents',
+        static_folder='static/',
+        url_prefix="/constituents"
+    )
+
+    repository = ConstituentRepository(connection=connection)
+    media_repository = MediaRepository(connection=connection)
+    constituent_attributes = [
+        "ulanid",
+        "preferred-display-name",
+        "forward-display-name",
+        "lastname",
+        "display-date",
+        "artist-of-nga-object",
+        "birthyear",
+        "deathyear",
+        "visualbrowsertimespan",
+        "nationality",
+        "constituent-type",
+        "wikidataid"
+    ]
+
+    @constituents.route('/', methods=['GET', 'POST'])
+    def constituents_page():
+        if request.method == 'POST':
+            if 'constituent-search' in request.form:
+                req = request.form['constituent-search']
+                return redirect( url_for('.constituent_by_name', name=req) )
+            elif 'add-constituent' in request.form:
+                req = request.form['add-constituent']
+                return redirect(url_for('.add_constituent'))
+        else:
+
+            constituents = repository.get_all_constituents()
+            constituent_id = request.args.get('constituent_id')
+            media = media_repository.get_constituent_media_by_id(constituent_id)
+            return render_template('constituents.html', constituents=constituents,  media=media, get_constituent_media_by_id=media_repository.get_constituent_media_by_id)
+    
+    @constituents.route('/<int:id>')
+    def constituent_by_id(id: int):
+        constituent = repository.get_constituent_by_id(id)
+        return f"{constituent.forwarddisplayname}"
+    
+    
+    @constituents.route('/<string:name>', methods=['GET', 'POST'])
+    def constituent_by_name(name: str):
+        constituents = repository.get_constituents_by_name(name=name)
+        if request.method == 'POST':
+            if 'constituent-search' in request.form:
+                req = request.form['constituent-search']
+                return redirect( url_for('.constituent_by_name', name=req) )
+            elif 'add-constituent' in request.form:
+                req = request.form['add-constituent']
+                return redirect(url_for('.add_constituent'))
+        return render_template('constituents.html', constituents=constituents)
+    
+    @constituents.route('/add', methods=['GET','POST'])
+    def add_constituent():
+        if request.method == 'GET':
+            return render_template('constituents_add.html')
+        else:
+            attributes = []
+            for key in constituent_attributes:
+                if key in request.form and len(request.form[key]):
+                    attributes.append(request.form[key])
+                else:
+                    attributes.append(None)
+            repository.add_constituent(attributes=attributes)
+            flash('Constituent has been added successfully.')
+            return render_template('constituents_add.html')
+
+    return constituents
---
+++ repository/media_repository.py
@@ -0,0 +1,91 @@
+class Media:
+    def __init__(self, mediaid, title, description, thumbnailurl, playurl, mediatype = None,duration = None,language = None,downloadurl = None, keywords = None,tags = None, imageurl =None,presentationdate = None, releasedate = None, lastmodified = None) -> None:
+        self.mediaid = mediaid
+        self.title = title
+        self.description = description
+        self.thumbnailurl = thumbnailurl
+        self.playurl = playurl
+
+
+class MediaRepository:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def get_constituent_media(self):
+        cursor = self.connection.cursor()
+
+        # Use LIMIT and OFFSET for pagination
+        query = f"SELECT thumbnailurl, title, description, playurl FROM constituents_media;"
+
+        cursor.execute(query)
+        media = cursor.fetchall()
+
+        return media
+  
+    def get_constituent_media_by_id(self, id):
+        if id is None:
+            return []  # Return an empty list or handle appropriately
+
+        cursor = self.connection.cursor()
+
+        query = '''
+            SELECT thumbnailurl
+            FROM constituents_media
+            WHERE mediaid IN (
+                SELECT mediaid
+                FROM media_relationships
+                WHERE relatedid = %s
+                AND BINARY relatedentity = 'nga:art:tms:constituents\r');
+        '''
+
+        cursor.execute(query, (id,))
+        media = cursor.fetchall()
+
+        return media
+
+    
+    def get_object_media(self, obj):
+        cursor = self.connection.cursor()
+
+        # Use parameterized queries to avoid SQL injection
+        query = '''
+            SELECT thumbnailurl
+                FROM object_media
+                WHERE mediaid IN (
+                    SELECT mediaid
+                    FROM media_relationships
+                    WHERE relatedid = %s
+                    AND BINARY relatedentity = 'nga:art:tms:objects\r');'''
+        cursor.execute(query, (obj.objectid,))
+        media = cursor.fetchall()
+
+        return media
+
+
+    
+    
+    def create_media(self, media):
+        media.mediaid = media.mediaid
+        return id
+
+    def add_media(self, media):
+        id = self.create_media(media)
+        cursor = self.connection.cursor()
+        query = '''INSERT INTO constituents_media (mediaid, title, description, thumbnailurl, playurl) VALUES(%s, %s, %s, %s, %s)'''
+        cursor.execute(query, (media.mediaid, media.title, media.description, media.thumbnailurl, media.playurl))
+        self.connection.commit()
+        return id
+    
+    def update_media(self, media):
+        print(media.mediaid)
+        cursor = self.connection.cursor()
+        query = '''UPDATE constituents_media SET title = %s, description = %s, thumbnailurl = %s, playurl = %s WHERE mediaid = %s'''
+        cursor.execute(query, (media.mediaid, media.title, media.description, media.thumbnailurl, media.playurl))
+        self.connection.commit()
+
+    def delete_media(self, media):
+        cursor = self.connection.cursor()
+        query = '''DELETE FROM constituents_media WHERE mediaid = %s'''
+        cursor.execute(query, [media.mediaid])
+        self.connection.commit() 
+
